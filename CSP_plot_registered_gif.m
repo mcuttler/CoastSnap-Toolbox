247bb18908fa9bbb0e1edf306ad53c6f8e360d17
mcuttler <michael.cuttler@uwa.edu.au>
2021-07-08 16:44:56 +0800

    updates for processing step 3

--- CSP_plot_registered_gif.m
+++ CSP_plot_registered_gif.m
@@ -13,66 +13,64 @@ tstart_vec= datevec(tstart);
 tend = datenum(time_end,'yyyy-mm-dd'); 
 tend_vec = datevec(tend); 
 
-for j = 1:length(sites)
-    %check if same year or not
-    if tend_vec(1)==tstart_vec(1)
-        impath = [base_path '\Images\' sites_db{j} '\Registered\' num2str(tstart_vec(1))]; 
-        dum = dir(impath);
-        dum = dum(3:end); 
-    else
-        impath = [base_path '\Images\' sites_db{j} '\Registered\' num2str(tstart_vec(1))];
-        impath2 = [base_path '\Images\' sites_db{j} '\Registered\' num2str(tend_vec(1))]; 
-        dum = dir(impath);        
-        dum = dum(3:end); 
-        dum2 = dir(impath2); 
-        dum2 = dum2(3:end); 
-        
-        dum = [dum; dum2]; 
-    end   
+%check if same year or not
+if tend_vec(1)==tstart_vec(1)
+    impath = [base_path '\Images\' sites_db '\Registered\' num2str(tstart_vec(1))]; 
+    dum = dir(impath);
+    dum = dum(3:end); 
+else
+    impath = [base_path '\Images\' sites_db '\Registered\' num2str(tstart_vec(1))];
+    impath2 = [base_path '\Images\' sites_db '\Registered\' num2str(tend_vec(1))]; 
+    dum = dir(impath);        
+    dum = dum(3:end); 
+    dum2 = dir(impath2); 
+    dum2 = dum2(3:end); 
     
-    %site outpath for GIF
-    outpath = [base_path '\Images\' sites_db{j} '\Timelapse']; 
-    if ~exist(outpath)
-        mkdir(outpath)
-    end
-    
-    %loop over images to make GIF 
-    [m,~] = size(dum);        
-    for i = 1:m
-        im_time = datetime(str2num(dum(i).name(1:10)), 'convertfrom','posixtime')+datenum(0,0,0,8,0,0);
-        if i == 1           
-            imshow([dum(i).folder '\' dum(i).name])
-            set(gcf, 'PaperPositionMode', 'manual','PaperUnits','centimeters','units','centimeters',...
-                'position',[4 2 19 16], 'PaperPosition', [0 0 19 16],'color','w')
-            set(gca,'units','centimeters'); 
-            set(gca,'Position',[0, -0.75, 19 16],'units','centimeters'); 
-            
-            text(0.05, 1.05,[sites{j} ' - ' datestr(im_time,'dd-mmm-yyyy')],'fontweight','bold','units','normalized','fontsize',14); 
-            
-            % Create a new gif file and write the first frame: 
-            gifname = [outpath '\' sites_db{j} '_' time_start '_' time_end '.gif'];
-            
-            gif(gifname,'DelayTime',0.2,'LoopCount',3,'frame',gcf);
-       
-        else
-            % Loop through every other frame.
-            imshow([dum(i).folder '\' dum(i).name])
-            set(gcf, 'PaperPositionMode', 'manual','PaperUnits','centimeters','units','centimeters',...
-                'position',[4 2 19 16], 'PaperPosition', [0 0 19 16],'color','w')
-            
-            set(gca,'units','centimeters'); 
-            set(gca,'Position',[0, -0.75, 19 16],'units','centimeters'); 
-            
-            text(0.05, 1.05,[sites{j} ' - ' datestr(im_time,'dd-mmm-yyyy')],'fontweight','bold','units','normalized','fontsize',14); 
-            gif
-            
-        end
-        
-    end
-    close(gcf)
+    dum = [dum; dum2]; 
 end
 
+%site outpath for GIF
+outpath = [base_path '\Images\' sites_db '\Timelapse']; 
+if ~exist(outpath)
+    mkdir(outpath)
 end
+
+%loop over images to make GIF
+[m,~] = size(dum);        
+for i = 1:m
+    im_time = datetime(str2num(dum(i).name(1:10)), 'convertfrom','posixtime')+datenum(0,0,0,8,0,0);
+    if i == 1           
+        imshow([dum(i).folder '\' dum(i).name])
+        set(gcf, 'PaperPositionMode', 'manual','PaperUnits','centimeters','units','centimeters',...
+            'position',[4 2 19 16], 'PaperPosition', [0 0 19 16],'color','w')
+        set(gca,'units','centimeters'); 
+        set(gca,'Position',[0, -0.75, 19 16],'units','centimeters'); 
+        
+        text(0.05, 1.05,[sites ' - ' datestr(im_time,'dd-mmm-yyyy')],'fontweight','bold','units','normalized','fontsize',14); 
+        
+        % Create a new gif file and write the first frame: 
+        gifname = [outpath '\' sites_db '_' time_start '_' time_end '.gif'];
+        
+        gif(gifname,'DelayTime',0.2,'LoopCount',3,'frame',gcf);
+        
+    else
+        % Loop through every other frame.
+        imshow([dum(i).folder '\' dum(i).name])
+        set(gcf, 'PaperPositionMode', 'manual','PaperUnits','centimeters','units','centimeters',...
+            'position',[4 2 19 16], 'PaperPosition', [0 0 19 16],'color','w')
+        
+        set(gca,'units','centimeters'); 
+        set(gca,'Position',[0, -0.75, 19 16],'units','centimeters'); 
+        
+        text(0.05, 1.05,[sites ' - ' datestr(im_time,'dd-mmm-yyyy')],'fontweight','bold','units','normalized','fontsize',14); 
+        gif
+        
+    end
+    
+end
+close(gcf)
+end
+
 
     
     
--- CSP_rename_registered_images.m
+++ CSP_rename_registered_images.m
@@ -15,12 +15,12 @@ tend = datenum(time_end,'yyyy-mm-dd');
 tend_vec = datevec(tend); 
 %check if same years 
 if tend_vec(1)==tstart_vec(1)
-    impath = [base_path '\Images\' sites_db{j} '\Registered\' num2str(tstart_vec(1))]; 
+    impath = [base_path '\Images\' sites_db '\Registered\' num2str(tstart_vec(1))]; 
     dum = dir(impath);
     dum = dum(3:end); 
 else
-    impath = [base_path '\Images\' sites_db{j} '\Registered\' num2str(tstart_vec(1))];
-    impath2 = [base_path '\Images\' sites_db{j} '\Registered\' num2str(tend_vec(1))]; 
+    impath = [base_path '\Images\' sites_db '\Registered\' num2str(tstart_vec(1))];
+    impath2 = [base_path '\Images\' sites_db '\Registered\' num2str(tend_vec(1))]; 
     dum = dir(impath);        
     dum = dum(3:end); 
     dum2 = dir(impath2); 
@@ -29,7 +29,7 @@ else
     dum = [dum; dum2]; 
 end
 
-%now rename 
+%now rename
 for i = 1:size(dum,1)
     if dum(i).name(1) == '_'
         name_out = dum(i).name(7:end-4); 
@@ -41,5 +41,6 @@ for i = 1:size(dum,1)
         delete(filein)
     end
 end
+
 
 end
