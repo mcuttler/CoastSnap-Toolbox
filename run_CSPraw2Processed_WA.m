4b8c7088435be270ca059bfe27d2631b52a6ed32
mcuttler <michael.cuttler@uwa.edu.au>
2022-11-28 12:39:01 +0800

    update download scripts

--- run_CSPraw2Processed_WA.m
+++ run_CSPraw2Processed_WA.m
@@ -1,9 +1,12 @@
 %% Convert raw imagery for CoastSnapWA to processed
 sites = {'binningup','bussojetty','dalyellup','eaton','koombanabay','shoalwater','prestonbeach','silversands'};
 
-CSPsetPaths; 
+CSPsetPaths;
+CSPloadPaths;
 
+disp('paths set, processing raw images'); 
 for i = 1:length(sites)
+
     CSPraw2Processed(sites{i});
 end
 
